7eef63ecee241c9b1638d2c89eac2897b8086996
Thaymer <thaymerapv@gmail.com>
2020-09-01 14:22:23 -0400

    fixs vulnerabilities

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ var app = express();
 app.use(forceSsl);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
