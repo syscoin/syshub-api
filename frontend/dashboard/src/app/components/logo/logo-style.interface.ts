d0c033edf5cf240d48afa3ef7d5819a30b70078d
Thaymer portillo <42485564+thaymerapv@users.noreply.github.com>
2020-12-29 07:38:18 +0100

    frontend dashboard admin removed, reason will be passed to syshub repo

--- frontend/dashboard/src/app/components/logo/logo-style.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface LogoStyle {
-    height: string;
-}
--- frontend/dashboard/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  appName:'Syscoin',
-  // apiBase:"https://198.211.117.144:3001",
-  apiBase:"https://thaymerportillo.com:3000",
-  production: true
-};
