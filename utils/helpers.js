3c876517263185ff045fb10ec31c579c217b0991
Thaymer portillo <42485564+thaymerapv@users.noreply.github.com>
2021-01-22 23:16:30 -0400

    new functions to use

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,27 +1,22 @@
 const { clientRPC } = require('./config');
 
-const checkDataMN = (mn) => {
-  if (RegExp(/-0|-1/).test(mn.txId) !== true) {
-    return false;
-  }
-  if (mn.name && mn.privateKey) {
-    return true;
-  }
-  return false;
-};
-
-const checkBodyEmpty = (body) => Object.keys(body).length === 0;
-
-// TODO test validation for necessary controllers in address
 const validateAddress = (address) => new Promise((resolve, reject) => {
   clientRPC.callRpc('validateaddress', [address]).call()
-    .then((res) => {
-      resolve(res);
+    .then(({ isvalid }) => {
+      resolve(isvalid);
     })
     .catch((err) => {
       reject(err);
     });
 });
+
+const checkDataMN = (address) => {
+  if (RegExp(/-0|-1/).test(address.txId) !== true) return false;
+  validateAddress(address.address).then((valid) => valid === true);
+  return !!(address.name && address.privateKey);
+};
+
+const checkBodyEmpty = (body) => Object.keys(body).length === 0;
 
 const componentToHex = (c) => {
   const hex = c.toString(16);
